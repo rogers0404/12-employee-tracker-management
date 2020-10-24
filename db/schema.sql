a9c23a9feceffde1ad739b51f92f3393b6209d21
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-10-23 20:57:17 -0400

    Created initial Structure for the project

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS db_company;
+CREATE DATABASE db_company;
+USE db_company;
+
+ CREATE TABLE department(
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL,
+  departmentId INTEGER,
+  PRIMARY KEY (id),
+  CONSTRAINT department_fk FOREIGN KEY (departmentId) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  firstName VARCHAR(30),
+  lastName VARCHAR(30),
+  roleId INTEGER,
+  managerId INTEGER,
+  PRIMARY KEY (id),
+  CONSTRAINT role_fk FOREIGN KEY (roleId) REFERENCES role(id),
+  CONSTRAINT manager_fk FOREIGN KEY (managerId) REFERENCES employee(id)
+);
+ /**/
---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (name) 
+VALUES  ('Sales'),
+        ('Engineering'),
+        ('Finance'),
+        ('Legal')
+);
+
+INSERT INTO role (title, Salary, departmentId) 
+VALUES  ('Sales Lead', 6000, 1),
+        ('Lead Engineer', 7500, 2),
+        ('Accouintant', 8000, 3),
+        ('Salesperson', 3500, 1),
+        ('Lawyer', 7500, 4),
+        ('Software Engineer', 6500, 2),
+        ('Legal Team Lead', 7500, 4)
+);
+
+INSERT INTO 
